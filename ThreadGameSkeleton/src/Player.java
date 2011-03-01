e1e5bb66073024f151374231b422b109cc173212
Chin Isaac-Heslop <cisaache@mines.edu>
2011-03-01 12:24:56 -0700

    tracked Player class and its bin folder...

---
+++ ThreadGameSkeleton/src/Player.java
@@ -0,0 +1,7 @@
+
+public class Player implements Runnable{
+private Location current; //keeps track of current location
+private int direction; //keeps track of current direction: 0 is up,1 is right,2 is down,3 is left
+private int point; //keeps track of number of points
+
+}
